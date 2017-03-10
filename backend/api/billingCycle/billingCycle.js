20d830e87d01ec784bb4d448c1adee99bf078956
Fábio Alves Ribeiro <alvesribeirof@gmail.com>
2017-03-10 15:40:22 -0300

    Revert "Alterando mensagens e organizando status"
    This reverts commit 3e75e7443a1862d1f9d78050977b7b337d733f3a.

--- backend/api/billingCycle/billingCycle.js
+++ backend/api/billingCycle/billingCycle.js
@@ -6,17 +6,17 @@ const mongoose = restful.mongoose;
 
 //Mapeamento de como será armazenado o credito no mongodb
 const creditSchema = new mongoose.Schema({
-    name: { type: String, required: [true, 'Informe o nome do crédito!'] },
-    value: { type: Number, min: 0, required: [true, 'Informe o valor do crédito!'] }
+    name: { type: String, required: [true, 'Informe o Nome do Crédito!'] },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do Crédito!'] }
 });
 
 //Mapeamento de como será armazenado o debito no mongodb
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: [true, 'Informe o nome do débito!'] },
+    name: { type: String, required: [true, 'Informe o nome do Débito!'] },
     value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: {
         type: String,
-        required: [false, 'Informe o status do débito!'],
+        required: [false, 'Informe o Status do débito!'],
         uppercase: false,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO']
     }
@@ -24,9 +24,9 @@ const debtSchema = new mongoose.Schema({
 
 //Mapeamento do schema do ciclo de pagamento e validação dos atributos
 const billingCycleSchema = new mongoose.Schema({
-    name: { type: String, required: [true, 'Informe o nome!'] },
-    month: { type: Number, min: 1, max: 12, required: [true, 'Informe o mês!'] },
-    year: { type: Number, min: 1970, max: 2500, required: [true, 'Informe o ano!'] },
+    name: { type: String, required: [true, 'Informe o Nome!'] },
+    month: { type: Number, min: 1, max: 12, required: [true, 'Informe o Mês!'] },
+    year: { type: Number, min: 1970, max: 2500, required: [true, 'Informe o Ano!'] },
     credits: [creditSchema], //array de créditos apontando para o esquema do créditos
     debts: [debtSchema] //array de débitos apontando para o esquema do débitos
 });
